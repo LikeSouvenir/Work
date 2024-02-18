147ab732e7f4ad6d8e46547a2ae4f40752b86576
MIkhail Vishniakov <grabilenko.yana@mail.ru>
2024-02-18 21:49:36 +0300

    Создал необходимые переменные

--- Контрольная_работа/Program.cs
+++ Контрольная_работа/Program.cs
@@ -11,8 +11,16 @@ namespace Контрольная_работа
         static void Main(string[] args)
         {
             string[] someArr = { "Hello", "2", "world", ":-)", "1234", "1567", "-2", "computer science" };
-            
-        
+            string[] answer = new string[someArr.Length];
+            int i = 0;
+            foreach (string three in someArr)
+            {
+                if (three.Length <= 3)
+                    answer[i] = three;
+                i++;
+            }
+            answer = answer.Where(x => x != null).ToArray();
+            Console.WriteLine(String.Join(",", answer));
         }
     }
 }
