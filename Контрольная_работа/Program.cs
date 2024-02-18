1d06d161b47f035f01d089c6077fabf6da9ab089
MIkhail Vishniakov <grabilenko.yana@mail.ru>
2024-02-18 21:33:15 +0300

    Создал массив

---
+++ Контрольная_работа/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Контрольная_работа
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] someArr = { "Hello", "2", "world", ":-)", "1234", "1567", "-2", "computer science" };
+            
+        
+        }
+    }
+}
